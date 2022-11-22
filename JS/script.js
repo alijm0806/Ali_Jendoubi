6b8bd10960c6cacf0932c6db8e49836670ebbe7d
alijm0806 <alijm1104@gmail.com>
2022-11-21 23:21:25 -0600

    Add instagram + indeed + github icons

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing", {
-  strings: ["", "Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+  strings: ["", "Full-stack Engineer", "Web Designer", "Web Developer", "Graphic Designer", "Programmer"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true
